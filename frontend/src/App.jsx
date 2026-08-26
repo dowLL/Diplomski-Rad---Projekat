a0d03496e74f27f2f0261a1533587fc2599ec649
Vladimir Vojnic <vladimirvojnic@gmail.com>
2026-08-26 21:09:12 +0200

    Postavka Projekta: Barcelona Tourism

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>Barcelona Tourism</h1>
+            <p>Discover the beauty of Barcelona.</p>
+        </div>
+    );
+}
+
+export default App;
